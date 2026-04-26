bc820fc268e8d6749f06ee2ae8a764117f6a3b68
Cardenas2911 <nicolas.cardenas1129@gmail.com>
2026-04-25 19:56:43 -0500

    Configurar GitHub Pages para Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cardenas2911.github.io',
+  base: '/prendautos',
   integrations: [react()],
 
   vite: {
